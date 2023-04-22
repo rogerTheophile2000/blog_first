1d79e1702d2800b89b4c29b8bdc17bb59cc49f15
Roger Mushagalusa <rogetheophile2000@gmail.com>
2023-04-22 06:22:27 +0300

    Adding crud operation in the app

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 # blog/views.py
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Post
+from django.urls import reverse_lazy
 
 
 
@@ -9,6 +11,21 @@ class BlogListView(ListView):
     model = Post
     template_name = "home.html"
 
-class BlogDetailView(DetailView): # new
+class BlogDetailView(DetailView):
     model = Post
     template_name = "post_detail.html"
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = "post_new.html"
+    fields = ["title", "author", "body"]
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = "post_edit.html"
+    fields = ["title", "body"]
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = "post_delete.html"
+    success_url = reverse_lazy("home")
